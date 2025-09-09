e030bcf5a36c973eef613d177668de8cf5d47a67
Bartłomiej Liber <liber.bartek@gmail.com>
2025-09-09 18:33:22 +0200

    feat: Add ensureAuthenticated and loadUser

---
+++ src/middlewares/auth.js
@@ -0,0 +1,4 @@
+export function ensureAuthenticated(req, res, next) {
+    if (req.session.userId) return next();
+    res.redirect('/auth/login');
+}
---
+++ src/middlewares/loadUser.js
@@ -0,0 +1,24 @@
+import User from "../models/User.js";
+
+export async function loadUser(req, res, next) {
+    const userId = req.session.userId;
+    if (!userId) {
+        res.locals.currentUser = null;
+        return next()
+    }
+
+    try {
+        const user = await User.findByPk(userId, {
+            attributes: ['id', 'email', 'username', 'role']
+        });
+        if (!user) {
+            req.session.destroy(() => {});
+            res.locals.currentUser = null;
+            return next();
+        }
+        res.locals.currentUser = user;
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+}
