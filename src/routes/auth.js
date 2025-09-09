68e0b21012d898bbeaa987b36463de8a10783c17
Bartłomiej Liber <liber.bartek@gmail.com>
2025-09-09 18:32:40 +0200

    feat: Add logout and register endpoints

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -17,11 +17,22 @@ r.post('/login', async (req, res) => {
     }
 
     req.session.userId = user.id;
-    req.session.user = {
-        id: user.id,
-        username: user.username,
-        role: user.role
-    };
+    res.redirect('/');
+});
+
+r.get('/logout', (req, res) => {
+    req.session.destroy(() => res.redirect('/login'));
+});
+
+r.get('/register', (req, res) => {
+    res.render('register', { title: 'Wypożyczalnia: Zarejestruj się' });
+});
+
+r.post('/register', async (req, res) => {
+    const { email, username, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = await User.create({ email, username, passwordHash, 'user' });
+    req.session.userId = user.id;
     res.redirect('/');
 });
 
