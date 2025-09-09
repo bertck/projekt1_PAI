1b659b39970cf6c4641c057c416fdbde2c173fc4
Bartłomiej Liber <liber.bartek@gmail.com>
2025-09-09 14:06:45 +0200

    chore: Refactoring

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import { join } from 'path';
 const app = express();
 
 // Pug setup
 app.set('view engine', 'pug');
 
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'PAI Projekt 1.', message: 'TEST'});
+    res.render('index', { title: 'PAI Projekt 1.', message: 'TEST' });
 });
 
 const PORT = 3000;
