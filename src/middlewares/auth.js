3b209caf3cb3de7f51baf4713420409c21ba6ae2
Bartłomiej Liber <liber.bartek@gmail.com>
2025-09-12 19:13:53 +0200

    feat: Add ensureAdmin middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,4 +1,18 @@
+import User from "../models/User";
+
 export function ensureAuthenticated(req, res, next) {
     if (req.session.userId) return next();
     res.redirect('/auth/login');
+}
+
+export function ensureAdmin(req, res, next) {
+    const userId = req.session.userId;
+    if (!userId) res.redirect('/auth/login');
+    try {
+        const user = User.findByPk(userId);
+        if (user && user.role == 'admin') return next();
+    } catch (error) {
+        return next(error);
+    }
+    res.status(403).send('Odmówiono dostępu!');
 }
