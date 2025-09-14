04a75be1c8adddb70418c654e71f4055e443aa31
Bartłomiej Liber <liber.bartek@gmail.com>
2025-09-14 21:15:36 +0200

    feat: Avoid duplicate DB query in ensureAdmin

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,18 +1,11 @@
-import User from "../models/User.js";
-
 export function ensureAuthenticated(req, res, next) {
     if (req.session.userId) return next();
     return res.redirect('/auth/login');
 }
 
-export async function ensureAdmin(req, res, next) {
-    const userId = req.session.userId;
-    if (!userId) return res.redirect('/auth/login');
-    try {
-        const user = await User.findByPk(userId);
-        if (user && user.role == 'admin') return next();
-    } catch (error) {
-        return next(error);
-    }
+export function ensureAdmin(req, res, next) {
+    const user = res.locals.currentUser;
+    if (!user) return res.redirect('/auth/login');
+    if (user.role === 'admin') return next();
     next({ status: 403, message: 'Odmówiono dostępu!' });
 }
