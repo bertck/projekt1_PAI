f853cf4f5e374d56e94858184488aed26762e61e
Bartłomiej Liber <liber.bartek@gmail.com>
2025-09-14 21:31:25 +0200

    security: Expand reservations' validations

--- src/routes/reservations.js
+++ src/routes/reservations.js
@@ -110,14 +110,26 @@ r.put('/:id', async (req, res, next) => {
         if (reservation.userId !== currentUser.id && currentUser.role !== 'admin') {
             return next({ status: 403, message: 'Odmówiono dostępu!' });
         }
-        const { machineId, startDate, endDate } = req.body;
+        const { startDate, endDate } = req.body;
+        const machineId = Number(req.body.machineId);
         const start = new Date(startDate);
         const end = new Date(endDate);
         const max = new Date();
         max.setMonth(max.getMonth() + 3);
-        if (!machineId || !startDate || !endDate || start > end || start > max || end > max) {
+        if (
+            !Number.isInteger(machineId) ||
+            !startDate ||
+            !endDate ||
+            isNaN(start) ||
+            isNaN(end) ||
+            start > end ||
+            start > max ||
+            end > max
+        ) {
             return res.status(400).render('error', { status: 400, message: INVALID_DATA_MSG });
         }
+        const machine = await Machine.findByPk(machineId);
+        if (!machine) return next({ status: 404, message: 'Nie znaleziono zasobu' });
         const conflict = await Reservation.findOne({
             where: {
                 id: { [Op.ne]: reservation.id },
